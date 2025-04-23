b19b3258a653e902c6521cb768a15ddb6a4664c2
BensaltanaHoussam <bensaltna07@gmail.com>
2025-04-23 12:16:02 +0100

    Refactor follower and following relationships to use hasMany instead of belongsToMany

--- database/migrations/2025_04_23_105125_create_follows_table.php
+++ database/migrations/2025_04_23_105125_create_follows_table.php
@@ -6,20 +6,19 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('follows', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('follower_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('following_id')->constrained('users')->onDelete('cascade');
             $table->timestamps();
+
+            // Prevent duplicate follows
+            $table->unique(['follower_id', 'following_id']);
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('follows');
