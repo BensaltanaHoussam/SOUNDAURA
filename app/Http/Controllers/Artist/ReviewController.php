ba9923ae68a7b2635a7e1644dcad9f2c81611d1c
BensaltanaHoussam <bensaltna07@gmail.com>
2025-04-29 10:27:23 +0100

    Implement index method in ReviewController to fetch and display user-specific albums with comments

--- app/Http/Controllers/Artist/ReviewController.php
+++ app/Http/Controllers/Artist/ReviewController.php
@@ -3,9 +3,17 @@
 namespace App\Http\Controllers\artist;
 
 use App\Http\Controllers\Controller;
+use App\Models\Album;
 use Illuminate\Http\Request;
 
 class ReviewController extends Controller
 {
-    //
+    public function index()
+    {
+        $albums = Album::where('user_id', auth()->id())
+            ->with(['comments.user'])
+            ->get();
+
+        return view('artist.reviews', compact('albums'));
+    }
 }
