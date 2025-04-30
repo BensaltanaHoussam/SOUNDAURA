35c9f1750bd1a5fb2b2e9f0cd90592640fadb107
BensaltanaHoussam <bensaltna07@gmail.com>
2025-04-30 20:30:31 +0100

    Refactor CommentController to increment artist aura upon new comment creation

--- app/Http/Controllers/listner/CommentController.php
+++ app/Http/Controllers/listner/CommentController.php
@@ -23,9 +23,16 @@ class CommentController extends Controller
             'user_id' => auth()->id()
         ]);
 
+
+        $album = Album::find($validated['album_id']);
+        $artist = $album->user;
+        if (auth()->id() !== $artist->id) {
+            $artist->increment('aura', 100);
+        }
+
         // Get the album and its artist
         $album = Album::findOrFail($validated['album_id']);
-        
+
         // Send notification if commenter is not the album owner
         if ($album->user_id !== auth()->id()) {
             $album->user->notify(new NewComment($comment, auth()->user()));
